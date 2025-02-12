54e991f4848824876eb399efd6181e46dd0bb589
KhingZaw <139222945+KhingZaw@users.noreply.github.com>
2025-02-13 02:44:14 +0630

    [add circularProgress in home page]

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,9 +1,28 @@
+import 'dart:async';
+
 import 'package:car_rental_app/widgets/about_card.dart';
 import 'package:car_rental_app/widgets/detail_card.dart';
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  bool isLoading = true;
+  @override
+  void initState() {
+    super.initState();
+    // Simulate a delay (e.g., fetching data)
+    Timer(Duration(seconds: 3), () {
+      setState(() {
+        isLoading = false; // Stop loading after 3 seconds
+      });
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -56,74 +75,81 @@ class HomePage extends StatelessWidget {
           ],
         ),
       ),
-      body: Padding(
-        padding: const EdgeInsets.only(left: 20, right: 20, top: 10),
-        child: Column(
-          children: [
-            DetailCard(),
-            SizedBox(
-              height: 20,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Container(
-                  width: 158,
-                  height: 180,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(20),
-                    // ignore: deprecated_member_use
-                    color: Colors.grey.withOpacity(0.2),
+      body: isLoading
+          ? Center(
+              child: CircularProgressIndicator(
+                color: Colors.black54,
+              ),
+            )
+          : Padding(
+              padding: const EdgeInsets.only(left: 20, right: 20, top: 10),
+              child: Column(
+                children: [
+                  DetailCard(),
+                  SizedBox(
+                    height: 20,
                   ),
-                  child: Padding(
-                    padding: const EdgeInsets.all(20),
-                    child: Column(
-                      children: [
-                        CircleAvatar(
-                          radius: 40,
-                          backgroundColor: Colors.grey.shade300,
-                          child: Image.asset("assets/images/card_image.png"),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Container(
+                        width: 158,
+                        height: 180,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(20),
+                          // ignore: deprecated_member_use
+                          color: Colors.grey.withOpacity(0.2),
                         ),
-                        SizedBox(
-                          height: 5,
+                        child: Padding(
+                          padding: const EdgeInsets.all(20),
+                          child: Column(
+                            children: [
+                              CircleAvatar(
+                                radius: 40,
+                                backgroundColor: Colors.grey.shade300,
+                                child:
+                                    Image.asset("assets/images/card_image.png"),
+                              ),
+                              SizedBox(
+                                height: 5,
+                              ),
+                              Text(
+                                "Jane Cooper",
+                                style: Theme.of(context).textTheme.titleSmall,
+                              ),
+                              SizedBox(
+                                height: 5,
+                              ),
+                              Text(
+                                "\$4.253",
+                                style: Theme.of(context).textTheme.titleMedium,
+                              ),
+                            ],
+                          ),
                         ),
-                        Text(
-                          "Jane Cooper",
-                          style: Theme.of(context).textTheme.titleSmall,
+                      ),
+                      Container(
+                        width: 158,
+                        height: 180,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(20),
+                          // ignore: deprecated_member_use
+                          color: Colors.grey.withOpacity(0.2),
+                          image: DecorationImage(
+                            image: AssetImage("assets/images/map.png"),
+                            fit: BoxFit.cover, // Adjust fit as needed
+                          ),
                         ),
-                        SizedBox(
-                          height: 5,
-                        ),
-                        Text(
-                          "\$4.253",
-                          style: Theme.of(context).textTheme.titleMedium,
-                        ),
-                      ],
-                    ),
+                      ),
+                    ],
                   ),
-                ),
-                Container(
-                  width: 158,
-                  height: 180,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(20),
-                    // ignore: deprecated_member_use
-                    color: Colors.grey.withOpacity(0.2),
-                    image: DecorationImage(
-                      image: AssetImage("assets/images/map.png"),
-                      fit: BoxFit.cover, // Adjust fit as needed
-                    ),
+                  SizedBox(
+                    height: 20,
                   ),
-                ),
-              ],
+                  AboutCard(),
+                ],
+              ),
             ),
-            SizedBox(
-              height: 20,
-            ),
-            AboutCard(),
-          ],
-        ),
-      ),
     );
   }
 }
